bf7766af5dff3b1949ed29c882a7956b92f50d9a
pangao <pangao@tuhu.cn>
2020-06-23 15:07:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="root"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+
+export default defineComponent({
+    name: 'App',
+    setup() {
+        const root = ref(null)
+        onMounted(() => {
+            console.log(root.value)
+        })
+    }
+})
+</script>
+<style lang="stylus">
+  #app {
+    img {
+      width: 100px;
+    }
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            count: 0
+        }
+    },
+    setup(props, ctx) {
+      console.log(props.msg)
+    }
+})
+</script>
